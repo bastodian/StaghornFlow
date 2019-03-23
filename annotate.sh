f4b477e42dd5f7e550847480dd21a0750e91fac1
Bastian Bentlage <bastodian@gmail.com>
2019-03-23 21:49:09 +1000

    Update annotate.sh

--- annotate.sh
+++ annotate.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 #Creates the necessary .pep file:
-#~/Transcriptome_Tools/TransDecoder-TransDecoder-v5.0.2/TransDecoder.LongOrfs -t /media/RAID/james/Staghorn_GeneExpression/Ref_Transcriptome_All/Filtered_Transcriptome/Trinity-GG.final.fasta
-#nice -n 6 ~/Transcriptome_Tools/TransDecoder-TransDecoder-v5.0.2/TransDecoder.Predict -t 
-/media/RAID/james/Staghorn_GeneExpression/Ref_Transcriptome_All/Trinity-GG.final.fasta
+~/Transcriptome_Tools/TransDecoder-TransDecoder-v5.0.2/TransDecoder.LongOrfs -t /media/RAID/james/Staghorn_GeneExpression/Ref_Transcriptome_All/Filtered_Transcriptome/Trinity-GG.final.fasta
+nice -n 6 ~/Transcriptome_Tools/TransDecoder-TransDecoder-v5.0.2/TransDecoder.Predict -t /media/RAID/james/Staghorn_GeneExpression/Ref_Transcriptome_All/Trinity-GG.final.fasta
 
 #Download the uniprot database you want with GO terms.
 # can do this with Kegg as well
@@ -13,11 +12,4 @@ makeblastdb -in uniprot-cnidariaGO.fasta -dbtype prot
 nice -n 6 blastx -query /media/RAID/james/Staghorn_GeneExpression/Ref_Transcriptome_All/Filtered_Transcriptome/Trinity-GG.final.fasta -db ./uniprot-cnidariakegg.fasta -max_hsps 1 -num_threads 40 -max_target_seqs 1 -outfmt 6 > keggblastx.outfmt6
 #We only used blastx for our annotations, but you could theoretically do blastp as well. 
 
-
-# the annotation output from trinotate is sparse and unhelpful, use the script ParseAnnotations.py instead once you have the blast 
-outputs
-
-
-
-
-
+# the annotation output from trinotate is sparse and unhelpful, use the script ParseAnnotations.py instead once you have the blast outputs
